8eccc7ffe09f7ed704b5b31dbd2af2c39ba1cbca
llAlcatrazll <alexismagaway456@gmail.com>
2025-09-23 17:40:01 +0800

    find Chronos

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 
-
+// asdasd
 app.use(cors({ origin: ['http://localhost:3000', 'https://discord-auth-token-bot.onrender.com'] }));
 // app.use(cors());
 app.use(express.json());
